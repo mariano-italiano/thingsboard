87c8df45219737a6f8901c9306d39953f493da1a
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-03-02 13:06:26 +0100

    Update deploy-ingress.sh

--- install/deploy-ingress.sh
+++ install/deploy-ingress.sh
@@ -3,3 +3,5 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
+
+kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": ["/nginx-ingress-controller","--election-id=ingress-nginx-leader","--controller-class=k8s.io/ingress-nginx","--ingress-class=nginx","--configmap=$(POD_NAMESPACE)/ingress-nginx-controller","--validating-webhook=:8443","--validating-webhook-certificate=/usr/local/certificates/cert","--validating-webhook-key=/usr/local/certificates/key","--watch-ingress-without-class=true","--enable-metrics=false","--publish-status-address=localhost","--enable-ssl-passthrough"]}]'
