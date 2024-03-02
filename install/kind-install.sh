a4a98e315f080bab02eb38aff4a94c46d7fa1b4d
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-03-02 13:58:28 +0100

    Update kind-install.sh

--- install/kind-install.sh
+++ install/kind-install.sh
@@ -82,3 +82,6 @@ else
 fi
 
 echo
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.0/components.yaml
+kubectl patch -n kube-system deployment metrics-server --type=json -p '[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
