bf25e31c450bf5d46535c59c9d75f22339a964fb
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-03-14 11:00:15 +0100

    Update deploy-ingress.sh

--- install/roles/deploy-k8s-tb/files/deploy-ingress.sh
+++ install/roles/deploy-k8s-tb/files/deploy-ingress.sh
@@ -11,4 +11,4 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/cont
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
 kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": ["/nginx-ingress-controller","--election-id=ingress-nginx-leader","--controller-class=k8s.io/ingress-nginx","--ingress-class=nginx","--configmap=$(POD_NAMESPACE)/ingress-nginx-controller","--validating-webhook=:8443","--validating-webhook-certificate=/usr/local/certificates/cert","--validating-webhook-key=/usr/local/certificates/key","--enable-ssl-passthrough"]}]'
 kubectl patch svc ingress-nginx-controller -n ingress-nginx --type='json' -p='[{"op": "replace", "path": "/spec/type", "value": "LoadBalancer"}]'
-kubectl patch svc backend -p '{"spec":{"externalIPs":["$1"]}}'
+kubectl patch svc ingress-nginx-controller -n ingress-nginx -p '{"spec":{"externalIPs":["'"$1"'"]}}'
